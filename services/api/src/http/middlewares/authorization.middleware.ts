3d6ede734a93f507cd42bc59834bfa3618679052
Gabriel Fernandes <gabrielfernandes.contact@gmail.com>
2024-11-22 14:12:51 -0300

    Init authorization header schema

--- services/api/src/http/middlewares/authorization.middleware.ts
+++ services/api/src/http/middlewares/authorization.middleware.ts
@@ -1,4 +1,11 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
+import { Type } from '@sinclair/typebox'
+
+export const authorizationHeaderSchema = Type.Object({
+  Authorization: Type.String({
+    pattern: '^Bearer\\s.+$',
+  }),
+})
 
 export async function isAuthorized(request: FastifyRequest, reply: FastifyReply): Promise<void> {
   if (!request.headers.authorization) {
